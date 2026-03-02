f60736b718f83c8f8f8a3f0c901fba86e560a233
jayelzaghmoutt <eng.jayel.z@gmail.com>
2026-03-03 00:21:14 +0200

    Support SPM
    - Implementations

--- Example/Pods/Target Support Files/JNMentionTextView/JNMentionTextView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_JNMentionTextView : NSObject
-@end
-@implementation PodsDummy_JNMentionTextView
-@end
---
+++ Sources/JNMentionTextView/PickerViewController/PickerViewControllerDelegate/JNMentionPickerViewControllerDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  JNMentionPickerViewControllerDelegate.swift
+//  JNMentionTextView
+//
+//  Created by JNDisrupter 💡 on 6/26/19.
+//
+
+import Foundation
+import UIKit
+
+// JNMention Picker View Delegate
+protocol JNMentionPickerViewControllerDelegate: NSObjectProtocol {
+    
+    /**
+     Get Cell
+     - Parameter item: Pickable item.
+     - Returns: UITableViewCell.
+     */
+    func jnMentionPickerViewController(cellFor item: JNMentionPickable) -> UITableViewCell
+    
+    /**
+     Height for cell
+     - Parameter item: Pickable item.
+     - Returns: UITableViewCell.
+     */
+    func jnMentionPickerViewController(cellHeightFor item: JNMentionPickable) -> CGFloat
+    
+    /**
+     Did Select Item
+     - Parameter indexPath: IndexPath.
+     */
+    func jnMentionPickerViewController(didSelectItemAt indexPath: IndexPath)
+}
